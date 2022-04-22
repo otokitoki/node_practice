cb09522df7f2f58dd179329084c262822a1faab1
toki-hayakawa <kaitoki03081122@gmail.com>
2022-04-23 00:43:28 +0900

    クエリパラメータ受け取ってリダイレクトできた

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/', (req, res) => {
+        res.send('ok');
+    })
+    .get('/youtube', (req, res) => {
+        const youtubeUrl = req.query.url;
+        console.log(youtubeUrl);
+        res.header('Refresh', '0; url=' + youtubeUrl);
+        res.send('ok');
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
